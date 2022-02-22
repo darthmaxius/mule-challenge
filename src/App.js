2dd6d9321dd26558924f2f2eae78856cf0d0b6b5
Maxi Silva <maximiliano.silva@gmail.com>
2022-02-22 12:26:30 -0300

    Format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { useState } from "react";
-import ReactFlow from "react-flow-renderer";
-import TextArea from "./components/textarea";
-import Button from "./components/button";
+import React, { useState } from 'react';
+import ReactFlow from 'react-flow-renderer';
+import TextArea from './components/textarea';
+import Button from './components/button';
 
-import "./App.scss";
+import './App.scss';
 
 export default function App() {
   const [elements, setElements] = useState([]);
-  const [textAreaValue, setTextAreaValue] = useState("");
+  const [textAreaValue, setTextAreaValue] = useState('');
   const [resume, setResume] = useState([]);
 
   return (
     <div className="layout">
       <div className="layout-panel">
         <div className="layout-panel__inputs">
-          <TextArea
-            className="layout-panel__inputs layout-panel__inputs-textarea"
-            hanlderApp={setTextAreaValue}
-          />
+          <TextArea className="layout-panel__inputs layout-panel__inputs-textarea" hanlderApp={setTextAreaValue} />
           <Button
             className="layout-panel__inputs layout-panel__inputs-button"
             textAreaValue={textAreaValue}
