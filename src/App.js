2e8bfd6862e9bfa635d91ad04370bafba96c456c
Maxi Silva <maximiliano.silva@gmail.com>
2022-02-22 12:15:02 -0300

    Minimun Layout OK

--- src/App.js
+++ src/App.js
@@ -3,43 +3,44 @@ import ReactFlow from "react-flow-renderer";
 import TextArea from "./components/textarea";
 import Button from "./components/button";
 
+import "./App.scss";
+
 export default function App() {
-  const elementsRaw = [
-    {
-      id: "1",
-      type: "input", // input node
-      data: { label: "Input Node" },
-      position: { x: 250, y: 25 },
-    },
-    // default node
-    {
-      id: "2",
-      // you can also pass a React component as a label
-      data: { label: <div>Default Node</div> },
-      position: { x: 100, y: 125 },
-    },
-    {
-      id: "3",
-      type: "output", // output node
-      data: { label: "Output Node" },
-      position: { x: 250, y: 250 },
-    },
-    // animated edge
-    { id: "e1-2", source: "1", target: "2", arrowHeadType: "arrowclosed" },
-    { id: "e2-3", source: "2", target: "3", arrowHeadType: "arrowclosed" },
-  ];
-
   const [elements, setElements] = useState([]);
-
-  useEffect(() => {
-    setElements(elementsRaw);
-  }, []);
+  const [textAreaValue, setTextAreaValue] = useState("");
+  const [resume, setResume] = useState([]);
 
   return (
-    <div>
-      <TextArea hanlderApp={setElements} />
-      <Button>Ajustar</Button>
-      <div style={{ height: "1000px" }}>
+    <div className="layout">
+      <div className="layout-panel">
+        <div className="layout-panel__inputs">
+          <TextArea
+            className="layout-panel__inputs layout-panel__inputs-textarea"
+            hanlderApp={setTextAreaValue}
+          />
+          <Button
+            className="layout-panel__inputs layout-panel__inputs-button"
+            textAreaValue={textAreaValue}
+            setElementsHandler={setElements}
+            setResumeHandler={setResume}
+          >
+            Draw
+          </Button>
+        </div>
+        {resume.length > 0 && (
+          <div className="layout-panel__resume">
+            <h2 className="layout-panel__resume-title">Resume</h2>
+            <ul className="layout-panel__resume-list">
+              {resume.map((line, key) => (
+                <li key={key}>
+                  {line.text} <b>{line.validated}</b>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      <div className="layout-graph">
         <ReactFlow elements={elements} />
       </div>
     </div>
--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Button({ children }) {
-  return <button>{children}</button>;
+function Button({
+  children,
+  setElementsHandler,
+  textAreaValue,
+  className,
+  setResumeHandler,
+}) {
+  const [counter, setCounter] = useState(0);
+
+  function formatString2Elements(input, outputs, uniqueElements) {
+    const newElements = [];
+
+    if (uniqueElements.indexOf(input) === -1) {
+      uniqueElements.push(input);
+
+      newElements.push({
+        id: `${counter}.e-${input}`,
+        data: { label: input },
+        position: { x: 250, y: 25 },
+      });
+    }
+
+    outputs.map((output, key) => {
+      const position = key * 10;
+      const positionX = 100 + position;
+      const positionY = 125 + position;
+
+      if (uniqueElements.indexOf(output) === -1) {
+        uniqueElements.push(output);
+
+        newElements.push({
+          id: `${counter}.e-${output}`,
+          // you can also pass a React component as a label
+          data: { label: output },
+          position: { x: positionX, y: positionY },
+        });
+      }
+
+      newElements.push({
+        id: `${counter}.e-${input}-e-${output}`,
+        source: `${counter}.e-${input}`,
+        target: `${counter}.e-${output}`,
+        arrowHeadType: "arrowclosed",
+        type: "step",
+      });
+    });
+
+    setCounter(counter + 1);
+
+    return newElements;
+  }
+
+  function insertText(e) {
+    const uniqueElements = [];
+    setElementsHandler([]);
+
+    const text = textAreaValue.trim();
+    const resume = [];
+
+    const lines = text.toUpperCase().split("\n");
+
+    const elements = [];
+
+    lines.map((line) => {
+      const auxInputOutputs = line.split("DEPENDS");
+
+      if (auxInputOutputs.length !== 2) {
+        resume.push({
+          text: line,
+          validated: "INVALID FORMAT",
+        });
+        return console.log(`THIS LINE ONLY HAVE ONE DEPENDS`);
+      }
+
+      const auxInput = auxInputOutputs[0].trim().split(" ");
+      const auxOutputs = auxInputOutputs[1].trim().split(" ");
+
+      console.log("INPUTS: ", auxInput, auxOutputs);
+
+      if (
+        auxInput.length > 1 ||
+        auxOutputs.length < 1 ||
+        auxOutputs[0] === ""
+      ) {
+        resume.push({
+          text: line,
+          validated: "INVALID LINE TO DRAW",
+        });
+        return console.log(`INVALID LINE TO DRAW "${line}"`);
+      }
+
+      console.log(`VALID STRING "${text}"`);
+      elements.push(
+        ...formatString2Elements(auxInput[0], auxOutputs, uniqueElements)
+      );
+
+      resume.push({
+        text: line,
+        validated: "OK",
+      });
+    });
+
+    console.log("DIBUJO CON VALORES: ", elements, uniqueElements);
+
+    setElementsHandler(elements);
+    setResumeHandler(resume);
+  }
+
+  return (
+    <button className={className} onClick={insertText}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
